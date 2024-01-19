5952e9c32a3b3a709b2666d1bbe041332a695114
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2024-01-20 04:17:59 +0530

    Freelancer page

--- freelance/src/components/freelancerPage/Freelancer.jsx
+++ freelance/src/components/freelancerPage/Freelancer.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import './Freelancer.css'
+import jobdes from '../JobDes/jobdes';
 
-function Freelancer() {
-    let jobList = []; // Assuming jobList is an array of job objects
+function Freelancer(props) {
 
     return (
         <div className='page'>
+            {/* <img src="/background.png" alt="" className="background" /> */}
             <div className="logo">DeLancer</div>
             <div className="profile">
                 <img src="" alt="" />
                 img
             </div>
-            <div className="tagLine">
-                Find the right freelance<br />service, right away
-            </div>
-            <div className="jobList">
-                {jobList.map((job) => (
-                    <div className="job" key={job.id}>
-                        <div className="jobTitle">{job.title}</div>
-                        <div className="jobDescription">{job.description}</div>
-                        <div className="jobPrice">{job.price}</div>
-                    </div>
-                ))}
+            <div className="content">
+                <div className="tagLine">
+                    Find the right freelance<br />service, right away
+                </div>
+                <div className="jobList">
+                    {props.jobList.map((job) => (
+                        <div className="job" key={job.id}>
+                            <div className="description">
+                                <div className="jobTitle">{job.title}</div>
+                                <p className="jobDescription">{job.description}</p>
+                            </div>
+                            <div className="price">
+                                <div className="jobPrice">${job.price}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
